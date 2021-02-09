fc7a2cdd03cceb24043956d278221d887f53a0b8
CRAZy-Monk3Y <61090388+CRAZy-Monk3Y@users.noreply.github.com>
2021-02-10 00:08:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,54 @@
-import pygame
-from datetime import datetime
-import time
-import random
+import pygame #IMPORT PYGAME
+from datetime import datetime #IMPORT DATETIME FUNCTION
+import time #IMPORT TIME
+import random #IMPORT RANDOM FUNCTION
 
-pygame.init()
+pygame.init() #INITIATING PYGAME
 
-white = (255, 255, 255)
+white = (255, 255, 255) #DEFINING ALL COLORS
 yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-dis_width = 800
+dis_width = 800 #DEFINING SCREEN DISPLAY DIMENSIONS
 dis_height = 600
 
-dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Tathagata.')
+dis = pygame.display.set_mode((dis_width, dis_height))  #ENABLING DISPLAY 
+pygame.display.set_caption('Snake Game by Tathagata.') #WRITING SCREEN CAPTION
 
-clock = pygame.time.Clock()
-# now = datetime.now()
+clock = pygame.time.Clock() #SETTING THE CLOCK
 start_time = time.time()
 snake_block = 10
 snake_speed = 10
 
-font_style = pygame.font.SysFont("bahnschrift", 25)
+font_style = pygame.font.SysFont("bahnschrift", 25) #DEFINING THE GAME FONTS
 score_font = pygame.font.SysFont("comicsansms", 35)
 time_font = pygame.font.SysFont("comicsansms", 35)
 
 
-def Your_score(score):
+def Your_score(score): #SCORE BLIT
     value = score_font.render("Your Score: " + str(score), True, yellow)
     dis.blit(value, [0, 0])
 
 
-def times(current_time):
+def times(current_time): #TIME BLIT
     value = time_font.render("Time -" + str(current_time), True, black)
     dis.blit(value, [540, 0])
 
 
-def our_snake(snake_block, snake_list):
+def our_snake(snake_block, snake_list): #SNAKE DEFINIG
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
 
-def message(msg, color):
+def message(msg, color): #MESSAGE PROMPMPT
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 8, dis_height / 3])
 
 
-def gameLoop():
+def gameLoop(): #MAIN GAME LOOP
     game_over = False
     game_close = False
 
